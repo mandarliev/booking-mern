277384cf8ac9294b1089cce8ee51e3c812cab932
Dimitar <dimitar.mandarliev@gmail.com>
2025-01-14 10:31:33 +0200

    mongodb setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/api/test", (req, res) => {
+app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "Hello from express endpoint" });
 });
 
