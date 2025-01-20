2b7be2f0276a3cb7d9a818070064f614e58c4002
Dimitar <dimitar.mandarliev@gmail.com>
2025-01-20 09:37:01 +0200

    add authentication routes with login functionality and validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.use("/api/users", userRoutes);
 
