32849239faba2da77569b0f60eea8dcd3ea20e93
Dimitar <dimitar.mandarliev@gmail.com>
2025-01-27 09:24:12 +0200

    add logout functionality with API endpoint and sign-out button component

---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,9 @@
+const SignOutButton = () => {
+  return (
+    <button className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-400">
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOutButton;
