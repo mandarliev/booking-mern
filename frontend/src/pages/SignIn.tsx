c4689e3663d6eded533144c65f669f5485d3a214
Dimitar <dimitar.mandarliev@gmail.com>
2025-01-27 08:35:37 +0200

    add sign-in functionality with form handling and success/error notifications

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,17 +1,40 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 export type SignInFormData = {
   email: string;
   password: string;
 };
 
 const SignIn = () => {
+  const { showToast } = useAppContext();
+  const navigate = useNavigate();
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<SignInFormData>();
 
+  const mutation = useMutation(apiClient.signIn, {
+    onSuccess: async () => {
+      // Show the toast
+      showToast({ message: "Sign in Successful!", type: "SUCCESS" });
+      // Navigate to the homepage
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Sign In</h2>
 
       <label className="text-gray-700 text-sm font-bold flex-1">
@@ -42,6 +65,14 @@ const SignIn = () => {
           <span className="text-red-500">{errors.password.message}</span>
         )}
       </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Login
+        </button>
+      </span>
     </form>
   );
 };
